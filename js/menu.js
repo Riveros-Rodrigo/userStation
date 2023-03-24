4cbe146d65d8c5e0d1f145ed20265a8fb4892452
Rodrigo Riveros <riverosdev@gmail.com>
2023-03-24 17:41:45 -0300

    mejoro page work y agrego menu mobile

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,21 @@
-const toggleMenuElement = document.getElementById('toggle-menu');
-const mainMenuElement = document.getElementById('main-menu');
+// Obtén el botón y el PopUp
+var btn = document.getElementById('myButton');
+var popup = document.getElementById('popup');
 
-toggleMenuElement.addEventListener('click', () => {
-mainMenuElement.classList.toggle('main-menu--show');
-});
+// Cuando el usuario hace clic en el botón, muestra el PopUp
+btn.onclick = function() {
+    popup.style.display = "block";
+}
+
+// Cuando el usuario hace clic en el botón de cerrar, oculta el PopUp
+var closeBtn = document.getElementsByClassName("close")[0];
+closeBtn.onclick = function() {
+    popup.style.display = "none";
+}
+
+// Cuando el usuario hace clic en cualquier lugar fuera del PopUp, oculta el PopUp
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
